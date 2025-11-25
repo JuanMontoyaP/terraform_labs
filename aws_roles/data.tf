01e23e373ae4a43bc5cfb7da383d0b1e7b5175cb
Juan P Montoya <84058843+JuanMontoyaP@users.noreply.github.com>
2025-11-25 14:55:12 -0500

    Fix roles

--- aws_roles/data.tf
+++ aws_roles/data.tf
@@ -1,53 +1,12 @@
-data "aws_iam_policy_document" "gh-actions-trust-policy" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Federated"
-      identifiers = [aws_iam_openid_connect_provider.gh-actions.arn]
-    }
-
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-
-    condition {
-      test     = "StringLike"
-      variable = "token.actions.githubusercontent.com:sub"
-      values = [
-        "repo:JuanMontoyaP/terraform_labs:ref:refs/heads/*",
-        "repo:JuanMontoyaP/terraform_labs:ref:refs/tags/*",
-        "repo:JuanMontoyaP/terraform_labs:pull_request"
-      ]
-    }
-  }
-}
-
 data "aws_iam_policy_document" "ec2-trust-policy" {
   statement {
     effect = "Allow"
 
     principals {
       type        = "AWS"
-      identifiers = [var.user_arn, aws_iam_role.gh-actions-role.arn]
+      identifiers = [var.user_arn, var.gh_actions_role_arn]
     }
 
     actions = ["sts:AssumeRole"]
-  }
-}
-
-data "aws_iam_policy_document" "gh-actions-tf-policy" {
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "s3:ListBucket",
-      "s3:GetObject",
-      "s3:PutObject",
-      "s3:DeleteObject",
-    ]
-
-    resources = [
-      "arn:aws:s3:::tf-labs-state-juan",
-      "arn:aws:s3:::tf-labs-state-juan/*",
-    ]
   }
 }
--- aws_roles/output.tf
+++ aws_roles/output.tf
@@ -1,8 +1,3 @@
-output "gh_role_arm" {
-  description = "The ARN of the GH actions role"
-  value       = aws_iam_role.gh-actions-role.arn
-}
-
 output "ec2_role_arn" {
   description = "The ARN of the EC2 role"
   value       = aws_iam_role.ec2_role.arn
